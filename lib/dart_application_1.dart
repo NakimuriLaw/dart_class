320e1187d5000606dc273de96273c62687682ad1
NakimuriLaw <violahnakimuri@gmail.com>
2023-07-10 16:28:38 +0300

    first commit

---
+++ lib/dart_application_1.dart
@@ -0,0 +1,23 @@
+// int calculate() {
+//   return 6 * 7 + 690 - 89;
+// }
+
+void main() {
+  // var name = "violah";
+  // String name2 = "violah";
+  // print(name);
+  // print(name2);
+
+  // print(name == name2);
+
+  var name = "Nakimuri_Violah";
+  var uni = "Bsu";
+  int age = 23;
+
+  print(name);
+  print(uni);
+  print(age);
+
+  print(
+      "I come from: $uni University & My name is : $name , I am : $age years old");
+}

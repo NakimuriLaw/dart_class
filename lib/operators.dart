73373a66915ed1777d8fd22edc42b697d786c784
NakimuriLaw <violahnakimuri@gmail.com>
2023-07-11 12:24:50 +0300

    introduction to althematic operators

---
+++ lib/operators.dart
@@ -0,0 +1,17 @@
+// void main() => print("welcome to operators class");
+void main() {
+  // print("welcome to class");
+  // String university = "Bishop Stuart University";
+  // print(university);
+  print(
+      "Arithmetic operators == these are orperators that do mathamtic operations");
+  int a, b;
+  a = 3;
+  b = 8;
+  print("addition: a + b =${a + b}"); //addition: a + b =11
+  print("subtraction: a - b =${a - b}"); //subtraction: a - b =-5
+  print("multiplicatin: a * b =${a * b}"); //multiplication: a * b =24
+  print("division: a / b =${a ~/ b}"); //division: a / b =0.375
+  print("division: a / b =${a / b}"); //division: a / b =0.375
+  print("modulus: a % b =${a % b}"); //modulus: a % b =3
+}

beea1c467ae836367c6dced25f083bb412d2fd04
NakimuriLaw <violahnakimuri@gmail.com>
2023-07-11 14:01:29 +0300

    logic operators

--- lib/operators.dart
+++ lib/operators.dart
@@ -3,15 +3,42 @@ void main() {
   // print("welcome to class");
   // String university = "Bishop Stuart University";
   // print(university);
-  print(
-      "Arithmetic operators == these are orperators that do mathamtic operations");
-  int a, b;
-  a = 3;
-  b = 8;
-  print("addition: a + b =${a + b}"); //addition: a + b =11
-  print("subtraction: a - b =${a - b}"); //subtraction: a - b =-5
-  print("multiplicatin: a * b =${a * b}"); //multiplication: a * b =24
-  print("division: a / b =${a ~/ b}"); //division: a / b =0.375
-  print("division: a / b =${a / b}"); //division: a / b =0.375
-  print("modulus: a % b =${a % b}"); //modulus: a % b =3
+  // print(
+  //     "Arithmetic operators == these are orperators that do mathamtic operations");
+  // int a, b;
+  // a = 3;
+  // b = 8;
+  // print("addition: a + b =${a + b}"); //addition: a + b =11
+  // print("subtraction: a - b =${a - b}"); //subtraction: a - b =-5
+  // print("multiplicatin: a * b =${a * b}"); //multiplication: a * b =24
+  // print("division: a / b =${a ~/ b}"); //division: a / b =0.375
+  // print("division: a / b =${a / b}"); //division: a / b =0.375
+  // print("modulus: a % b =${a % b}"); //modulus: a % b =3
+  // // comparison operators
+  // print(a < b);
+  // print(a == b);
+  // print("bsu" == "bsu");
+  // print(2 <= 3);
+  // print(a == b);
+
+  // if (university == "Bishop Stuart University") {
+  //   print("I am a student of BSU");
+  // }
+  // int age = 18;
+  // if (age > 18) {
+  //   print("old");
+  // } else if (age == 18) {
+  //   print("am just child");
+  // } else {
+  //   print("yonger");
+// }
+  int age = 23;
+  bool drinks = true;
+  if (age > 18) {
+    print("allowed1");
+  } else if (drinks) {
+    print("allowed2");
+  } else {
+    print("Nt allowed");
+  }
 }

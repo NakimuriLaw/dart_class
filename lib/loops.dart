402dc44ec9ac8efb098da3868d34a7da0aece9b7
NakimuriLaw <violahnakimuri@gmail.com>
2023-07-12 13:25:34 +0300

    added lists on our program

--- lib/loops.dart
+++ lib/loops.dart
@@ -1,22 +1,12 @@
+// while loops
+// syntax
+// while(condition){statement}
 void main() {
-  var list = [1, 2, 3, 4];
-  List<dynamic> mixedList = ["1", 3, "four", true, false];
-  List<String> strings = ["BSU", "1", "2"];
-  List<int> numbers = [1, 2, 3, 4];
-  List<bool> booleans = [true, false];
-  List<List<int>> numberList = [
-    [1, 3, 4],
-    [3, 5, 5],
-    [10, 6, 7],
-  ];
-  print(booleans.reversed);
-  print(numbers.reversed);
-  print(strings.contains("love"));
-  print(list.first);
-  print(list[0] == list.first);
-  print(mixedList.length);
-  print(numberList.lastOrNull);
-  print(strings[0][0]);
-  print(strings[0][1]);
-  print(strings[0][2]);
+  List<int> myNumbers = [];
+  int i = 10;
+  while (i <= 20) {
+    myNumbers.add(i);
+    i = i + 1;
+  }
+  print(myNumbers);
 }

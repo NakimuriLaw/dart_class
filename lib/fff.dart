45e7145a14225040bc151f33bc5b66cd965a26f5
NakimuriLaw <violahnakimuri@gmail.com>
2023-07-14 13:48:57 +0300

    got to commit every day

---
+++ lib/fff.dart
@@ -0,0 +1,5 @@
+import 'package:dart_application_1/functions.dart';
+
+void main() {
+  getAge();
+}
--- lib/functions.dart
+++ lib/functions.dart
@@ -23,6 +23,23 @@ void getAge() {
   print("i am $age years old");
 }
 
+// String getName2()=>print("getName2 function")
+String getName2() {
+  // print("getName2 function");
+  return "Lawvio";
+}
+
+String getmyName() {
+  print("who are you");
+  String? name = stdin.readLineSync();
+  // print("My name is $name");
+  // if (name != null) {
+  //   return name;
+  // }
+  // return "";
+  return name ?? "";
+}
+
 void main() {
   // print("who are you");
   // String? name = stdin.readLineSync();
@@ -32,8 +49,6 @@ void main() {
   // var age = stdin.readLineSync();
   // print(age!.toUpperCase());
   // myFunc();
-  getUserName();
-  // print("");
   // getUserName();
   // print("");
   // getUserName();
@@ -41,6 +56,12 @@ void main() {
   // getUserName();
   // print("");
   // getUserName();
-  getAge();
-  print("my name is $globalName and i am $globalAge years old ");
+  // print("");
+  // getUserName();
+  // getAge();
+  // print("my name is $globalName and i am $globalAge years old ");
+  // String myName = getName2();
+  // print(myName);
+  String myName = getmyName();
+  print("Your name is ${myName.toUpperCase()}");
 }
